52d8d2afdd6d3cdfc9bd0c2932e32775124781f8
nan <nan>
2023-12-25 14:25:05 +0530

    Fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,12 +2,19 @@ import styled from 'styled-components';
 
 const Overlay = styled.div`
   position: fixed;
-  inset: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: rgba(0, 0, 0, 0.5);
+  overflow-x: auto;
 `;
 
 const ModalBody = styled.div`
-  margin: 1rem auto;
+  width: 100%;
   max-width: 500px;
 
   @media screen and (max-width: 375px) {
